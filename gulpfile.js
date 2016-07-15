ef057315ca8af7f3c6e393e10b203278c6eea535
SHEFFcode <jeremy.shefer@foresee.com>
2016-07-15 10:36:07 -0700

    asdadasda

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulpif = require('gulp-if');
 gulp.task('add', function () {
   return gulp.src('*.js', {read: false})
   .pipe(gulpif(!(fs.exists('./v1.1/')), shell([
-    'git remote add -f my-lib https://github.com/SHEFFcode/subrepo.git; git subtree add --prefix my-lib my-lib <%= g %>'
+    'git remote add -f my-lib https://github.com/SHEFFcode/subrepo.git -b<%= g %>; git subtree add --prefix my-lib my-lib <%= g %>'
   ], {
     templateData: {
       g: 'v1.1'
