3f2919fb913dd619b198d6124945ddb1b0dd073f
SHEFFcode <jeremy.shefer@foresee.com>
2016-07-14 23:29:40 -0700

    modify the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulpif = require('gulp-if');
 gulp.task('example', function () {
   return gulp.src('*.js', {read: false})
   .pipe(gulpif(!(fs.exists('./v1.1/')), shell([
-    'git subrepo clone https://github.com/SHEFFcode/subrepo.git <%= g %> -b <%= g %>'
+    'git subtree add --prefix my-lib my-lib <%= g %>'
   ], {
     templateData: {
       g: 'v1.1'
