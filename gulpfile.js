6aac788b219d6b56c6c7c88a7c46179b53129925
SHEFFcode <sheff@sheffmachine.com>
2016-07-15 12:16:50 -0700

    update the fs.exists to search for my-lib folder

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulpif = require('gulp-if');
 
 gulp.task('add', function () {
   return gulp.src('*.js', {read: false})
-  .pipe(gulpif(!(fs.exists('./v1.1/')), shell([
+  .pipe(gulpif(!(fs.exists('./my-lib/')), shell([
     'git remote add -f my-lib https://github.com/SHEFFcode/subrepo.git; git subtree add --prefix my-lib my-lib <%= g %>'
   ], {
     templateData: {
